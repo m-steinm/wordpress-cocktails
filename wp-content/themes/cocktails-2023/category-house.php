50f037cb2a77dc4c4c1c070d25c29ceaec5ef728
Max <steinmetz.max@gmail.com>
2024-01-30 11:54:54 -0500

    Add more detail pages

--- wp-content/themes/cocktails-2023/category-house.php
+++
@@ -1,8 +0,0 @@
-
-<?php get_header();?>
-
-<?php
-	$term = get_field('category');
-	$house = get_field('house', $term);
-?>
-
--- wp-content/themes/cocktails-2023/templates/pages/home.php
+++
@@ -1,5 +0,0 @@
-<section class="page-section">
-	<inner-column>
-		<h1>This is the home page</h1>
-	</inner-column>
-</section>
